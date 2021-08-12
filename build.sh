c29a2241560a8faada55c87d27580470aa190f97
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-12 10:11:09 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ git pull
 nasm -f elf32 kernel.asm -o kasm.o
 gcc -fno-stack-protector -m32 -c kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
+echo "Build Complete. Now running in QEMU."
 qemu-system-i386 -no-reboot -kernel kernel
