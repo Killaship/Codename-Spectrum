c030b1ca5f8630751e32bb3bbf36b157a9155e4e
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-15 08:58:53 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Assembled..."
 gcc -Wall -fno-stack-protector -m32 -c kernel.c -o kc.o
 echo "Compiled..."
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
-"Linked..."
+echo "Linked..."
 echo "Build Complete. Now running in QEMU."
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
 qemu-system-i386 -no-reboot -kernel kernel
