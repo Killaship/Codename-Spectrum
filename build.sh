52ed0d5e865f183ac713c03393a5e3dd748004c6
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-15 11:22:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 echo "NOTE: This requires QEMU installed, and an X server as well."
 echo "Now Building..."
 git pull
-nasm -f elf32 kernel.asm -o kasm.o
-gcc -Wall -fno-stack-protector -m32 -c kernel.c -o kc.o
+nasm -f elf32 kernel/kernel.asm -o kasm.o
+gcc -Wall -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
 echo "Build Complete. Now running in QEMU."
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
