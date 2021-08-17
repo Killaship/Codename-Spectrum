046108046e21c0d13e94257d04fed2c373394b1a
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-17 09:22:49 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ git pull
 nasm -f elf32 kernel/kernel.asm -o kasm.o
 gcc -Wall -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o -r loadableimg.o kasm.o kc.o
-echo "Build Complete. Now running in QEMU."
-export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -no-reboot -kernel kimage
+echo "Build Complete. To use, link output file with Photon Bootloader."
+
