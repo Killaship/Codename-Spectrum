eeafa5307537d3b3e492bcc0946a9f2602d0caeb
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 07:26:03 -0400

    Delete kernel.h

--- kernel/kernel.h
+++
@@ -1,7 +0,0 @@
-void kprint(const char *str, const int color);
-void kprint_newline(void);
-void clear_screen(void);
-char read_port(unsigned short port);
-void write_port(unsigned short port, unsigned char data);
-void disable_ints(void);
-void enable_ints(void);
