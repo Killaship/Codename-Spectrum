9cd912b4152975794abd29be53398e0c72673d04
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 07:44:16 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o SKernel kasm.o kc.o
 echo "Build Complete. Now running in QEMU."
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -no-reboot -kernel Skernel
+qemu-system-i386 -no-reboot -kernel SKernel
