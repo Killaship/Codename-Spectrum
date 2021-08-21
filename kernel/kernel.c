f97b77cb1ab7aae1f340fa0f30759c6dbf205f29
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 08:50:50 -0400

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,8 +4,8 @@
 */
 
 #include "keyboard_map.h"
+#include "tty.h"
 #include "panic.h"
-
 
 
 #define LINES 25
@@ -36,9 +36,7 @@ extern void disable_ints();
 
 
 /* current cursor location */
-unsigned int current_loc = 0;
-/* video memory begins at address 0xb8000 */
-char *vidptr = (char*)0xb8000;
+
 
 struct IDT_entry {
 	unsigned short int offset_lowerbits;
@@ -49,31 +47,6 @@ struct IDT_entry {
 };
 
 
-void kprint(const char *str, const int color) {
-	
-	unsigned int i = 0;
-	while (str[i] != '\0') {
-		vidptr[current_loc++] = str[i++];
-		vidptr[current_loc++] = color;
-	}
-}
-
-
-
-void kprint_newline(void)
-{
-	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-	current_loc = current_loc + (line_size - current_loc % (line_size));
-}
-
-void clear_screen(void) {
-	unsigned int i = 0;
-	while (i < SCREENSIZE) {
-		vidptr[i++] = ' ';
-		vidptr[i++] = 0x07;
-	}
-
-}
 
 
 
