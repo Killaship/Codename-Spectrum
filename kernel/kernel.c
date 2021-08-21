d9dd77a664fce1326d7674b22001881e1c2cf1c2
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 08:55:40 -0400

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -240,7 +240,7 @@ void kmain(void) {
 	kprint("0",0xEE);
 	kprint("0",0xFF);
 	kprint_newline();
-	const char *str = "Codename Spectrum Build 0.4.0";
+	const char *str = "Codename Spectrum Build 0.4.2?";
 	const char *str2 = "                        Now with %66 more .h files!";
 	kprint(str, 0x0B);
 	kprint_newline();
