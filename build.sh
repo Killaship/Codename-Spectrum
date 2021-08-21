c6a6f588ad70ce6393327ea0db882bf68c62944f
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 07:32:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm kasm.o
 git pull
 nasm -f elf32 kernel/kernel.asm -o kasm.o
 gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
-ld -m elf_i386 -T link.ld -o kimage kasm.o kc.o
+ld -m elf_i386 -T link.ld -o SKernel kasm.o kc.o
 echo "Build Complete. Now running in QEMU."
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -no-reboot -kernel kimage
+qemu-system-i386 -no-reboot -kernel Skernel
