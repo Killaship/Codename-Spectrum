550e851a4b577cdf620242a2f2b6a6b96f662ad7
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 08:51:24 -0400

    Update tty.h

--- kernel/tty.h
+++ kernel/tty.h
@@ -1,4 +1,10 @@
 #pragma once
+
+#define LINES 25
+#define COLUMNS_IN_LINE 80
+#define BYTES_FOR_EACH_ELEMENT 2
+#define SCREENSIZE BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
+
 /* current cursor location */
 unsigned int current_loc = 0;
 /* video memory begins at address 0xb8000 */
