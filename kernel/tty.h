d7d508010b39b585da8e7f1f0a0baa7493fb78c9
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 08:50:44 -0400

    Create tty.h

---
+++ kernel/tty.h
@@ -0,0 +1,31 @@
+#pragma once
+/* current cursor location */
+unsigned int current_loc = 0;
+/* video memory begins at address 0xb8000 */
+char *vidptr = (char*)0xb8000;
+
+void kprint(const char *str, const int color) {
+	
+	unsigned int i = 0;
+	while (str[i] != '\0') {
+		vidptr[current_loc++] = str[i++];
+		vidptr[current_loc++] = color;
+	}
+}
+
+
+
+void kprint_newline(void)
+{
+	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+	current_loc = current_loc + (line_size - current_loc % (line_size));
+}
+
+void clear_screen(void) {
+	unsigned int i = 0;
+	while (i < SCREENSIZE) {
+		vidptr[i++] = ' ';
+		vidptr[i++] = 0x07;
+	}
+
+}
