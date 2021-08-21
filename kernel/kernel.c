39df9b762f1faa217b0935ae623f71a70d42f9ca
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 07:42:01 -0400

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -33,9 +33,7 @@ extern void write_port(unsigned short port, unsigned char data);
 extern void enable_ints();
 extern void disable_ints();
 
-void div0_handler(void) {
-	kprint("A division by 0 has occured. Please run around in circles.",0x04);
-}
+
 /* current cursor location */
 unsigned int current_loc = 0;
 /* video memory begins at address 0xb8000 */
@@ -48,6 +46,37 @@ struct IDT_entry {
 	unsigned char type_attr;
 	unsigned short int offset_higherbits;
 };
+
+
+void kprint(const char *str, const int color) {
+	
+	unsigned int i = 0;
+	while (str[i] != '\0') {
+		vidptr[current_loc++] = str[i++];
+		vidptr[current_loc++] = color;
+	}
+}
+
+
+
+void kprint_newline(void)
+{
+	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+	current_loc = current_loc + (line_size - current_loc % (line_size));
+}
+
+void clear_screen(void) {
+	unsigned int i = 0;
+	while (i < SCREENSIZE) {
+		vidptr[i++] = ' ';
+		vidptr[i++] = 0x07;
+	}
+
+}
+
+void div0_handler(void) {
+	kprint("A division by 0 has occured. Please run around in circles.",0x04);
+}
 
 struct IDT_entry IDT[IDT_SIZE];
 /* TODO: finish idt entry generator and replace all entries with it
@@ -146,31 +175,6 @@ void kb_init(void)
 	write_port(0x21 , 0xFD);
 }
 
-void kprint(const char *str, const int color) {
-	
-	unsigned int i = 0;
-	while (str[i] != '\0') {
-		vidptr[current_loc++] = str[i++];
-		vidptr[current_loc++] = color;
-	}
-}
-
-// strlen strcpy strcat
-
-void kprint_newline(void)
-{
-	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-	current_loc = current_loc + (line_size - current_loc % (line_size));
-}
-
-void clear_screen(void) {
-	unsigned int i = 0;
-	while (i < SCREENSIZE) {
-		vidptr[i++] = ' ';
-		vidptr[i++] = 0x07;
-	}
-
-}
 
 
 
