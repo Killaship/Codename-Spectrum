b4a81adfec139e76a3c9e7e39d6678537e5afdce
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-23 16:14:04 -0400

    Update panic.h

--- kernel/panic.h
+++ kernel/panic.h
@@ -111,3 +111,31 @@ void boundrx_handler() {
           );	
 }
 
+void dfault_handler() {
+	//kprint_newline();
+	unsigned int i = 0;
+	while (i < SCREENSIZE) {
+		vidptr[i++] = ' ';
+		vidptr[i++] = 0x44;
+	}
+	kprint("err: kernel panic!",0x40);
+	kprint_newline();
+	kprint("err type:",0x40);
+	kprint_newline();
+	kprint("abort: double fault (0x08)",0x40);
+	kprint_newline();
+	kprint_newline();
+	kprint_newline();
+	kprint_newline();
+	kprint("Please contact the devs and send them a screenshot.",0x40)
+	kprint_newline();
+	kprint("This is a pretty rare error, how'd you manage to screw up the kernel this much?!",0x40);
+	asm volatile(
+          "1:\n\t"
+          "cli\n\t"
+          "hlt\n\t"
+          "jmp 1b\n\t"
+          );	
+}
+
+
