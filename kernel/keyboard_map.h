2b393ab13b7b42795b5ce646f62acb49d583fd5f
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-13 17:26:57 -0500

    Update keyboard_map.h

--- kernel/keyboard_map.h
+++ kernel/keyboard_map.h
@@ -1,184 +1,39 @@
-unsigned char qwertylow[128] = {
-	0, // NULL
-	27, // ESC
-	'1',
-	'2',
-	'3',
-	'4',
-	'5',
-	'6',
-	'7',
-	'8',
-	'9',
-	'0',
-	'-',
-	'=',
-	'\b', // BACKSPACE
-	'\t', // TAB
-	'q',
-	'w',
-	'e',
-	'r',
-	't',
-	'y',
-	'u',
-	'i',
-	'o',
-	'p',
-	'[',
-	']',
-	'\n', // ENTER
-	0, // LEFT CONTROL
-	'a',
-	's',
-	'd',
-	'f',
-	'g',
-	'h',
-	'j',
-	'k',
-	'l',
-	';',
-	'\'',
-	'`',
-	0, // LEFT SHIFT
-	'\\',
-	'z',
-	'x',
-	'c',
-	'v',
-	'b',
-	'n',
-	'm',
-	',',
-	'.',
-	'/',
-	0, // RIGHT SHIFT
-	0, // KEYPAD *
-	0, // LEFT ALT
-	' ',
-	0, // CAPS LOCK
-	0, // F1
-	0, // F2
-	0, // F3
-	0, // F4
-	0, // F5
-	0, // F6
-	0, // F7
-	0, // F8
-	0, // F9
-	0, // F10
-	0, // NUM LOCK
-	0, // SCROLL LOCK
-	0, // KEYPAD 7
-	0, // KEYPAD 8
-	0, // KEYPAD 9
-	0, // KEYPAD -
-	0, // KEYPAD 4
-	0, // KEYPAD 5
-	0, // KEYPAD 6
-	0, // KEYPAD +
-	0, // KEYPAD 1
-	0, // KEYPAD 2
-	0, // KEYPAD 3
-	0, // KEYPAD 0
-	0, // KEYPAD .
-	0, // EMPTY
-	0, // EMPTY
-	0, // EMPTY
-	0, // F11
-	0, // F12
-};
-
-// ###### QWERTY UPPER CASE #######
-unsigned char qwertyup[128] = {
-	0, // NULL
-	27, // ESC
-	'!',
-	'@',
-	'#',
-	'$',
-	'%',
-	'^',
-	'&',
-	'*',
-	'(',
-	')',
-	'_',
-	'+',
-	'\b', // BACKSPACE
-	'\t', // TAB
-	'Q',
-	'W',
-	'E',
-	'R',
-	'T',
-	'Y',
-	'U',
-	'I',
-	'O',
-	'P',
-	'{',
-	'}',
-	'\n', // ENTER
-	0, // LEFT CONTROL
-	'A',
-	'S',
-	'D',
-	'F',
-	'G',
-	'H',
-	'J',
-	'K',
-	'L',
-	':',
-	'"',
-	'~',
-	0, // LEFT SHIFT
-	'|',
-	'Z',
-	'X',
-	'C',
-	'V',
-	'B',
-	'N',
-	'M',
-	'<',
-	'>',
-	'?',
-	0, // RIGHT SHIFT
-	0, // KEYPAD *
-	0, // LEFT ALT
-	' ',
-	0, // CAPS LOCK
-	0, // F1
-	0, // F2
-	0, // F3
-	0, // F4
-	0, // F5
-	0, // F6
-	0, // F7
-	0, // F8
-	0, // F9
-	0, // F10
-	0, // NUM LOCK
-	0, // SCROLL LOCK
-	0, // KEYPAD 7
-	0, // KEYPAD 8
-	0, // KEYPAD 9
-	0, // KEYPAD -
-	0, // KEYPAD 4
-	0, // KEYPAD 5
-	0, // KEYPAD 6
-	0, // KEYPAD +
-	0, // KEYPAD 1
-	0, // KEYPAD 2
-	0, // KEYPAD 3
-	0, // KEYPAD 0
-	0, // KEYPAD .
-	0, // EMPTY
-	0, // EMPTY
-	0, // EMPTY
-	0, // F11
-	0, // F12
+unsigned char keyboard_map[128] =
+{
+0, 27, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
+'9', '0', '-', '=', '\b', /* Backspace */
+'\t', /* Tab */
+'q', 'w', 'e', 'r', /* 19 */
+'t', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+0, /* 29 - Control */
+'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', /* 39 */
+'\'', '`', 0, /* Left shift */
+'\\', 'z', 'x', 'c', 'v', 'b', 'n', /* 49 */
+'m', ',', '.', '/', 0, /* Right shift */
+'*',
+0, /* Alt */
+' ', /* Space bar */
+0, /* Caps lock */
+0, /* 59 - F1 key ... > */
+0, 0, 0, 0, 0, 0, 0, 0,
+0, /* < ... F10 */
+0, /* 69 - Num lock*/
+0, /* Scroll Lock */
+0, /* Home key */
+0, /* Up Arrow */
+0, /* Page Up */
+'-',
+0, /* Left Arrow */
+0,
+0, /* Right Arrow */
+'+',
+0, /* 79 - End key*/
+0, /* Down Arrow */
+0, /* Page Down */
+0, /* Insert Key */
+0, /* Delete Key */
+0, 0, 0,
+0, /* F11 Key */
+0, /* F12 Key */
+0, /* All other keys are undefined */
 };
