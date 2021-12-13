b31274cb9752e14b1adf52c4d93decaccd770dcb
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-13 18:14:38 -0500

    Update rpibuild.sh

--- rpibuild.sh
+++ rpibuild.sh
@@ -5,8 +5,8 @@ rm kasm.o
 git pull
 #nasm -f elf32 boot/boot.asm -o boot.o
 nasm -f elf32 kernel/kernel.asm -o kasm.o
-clang -target  i686-unknown-none -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o 
-ldd -T link.ld -o kernel.elf kasm.o kc.o #boot.o
+clang -target  i686-unknown-none -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -fuse-ld=gold -m elf_i386 -T link.ld -o kernel.elf kasm.o kc.o #boot.o
+
 echo "Build Complete. Now running in QEMU."
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
 qemu-system-i386 -kernel kernel.elf
