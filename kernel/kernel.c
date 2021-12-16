b1fcb72ba2917158200ba106c03217186ea66e2a
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-15 20:41:26 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -23,6 +23,7 @@
 //extern void div0_handler(void);
 //extern void overf_handler(void);
 extern void keyboard_handler(void);
+//extern void pit_handler(void);
 extern void load_idt(unsigned long *idt_ptr);
 extern char read_port(unsigned short port);
 extern void write_port(unsigned short port, unsigned char data);
@@ -66,7 +67,7 @@ void idt_init(void) {
 	
 	
 	unsigned long keyboard_address;
-	unsigned long pit_address;
+	//unsigned long pit_address;
  
 	unsigned long idt_address;
 	unsigned long idt_ptr[2];
@@ -78,14 +79,14 @@ void idt_init(void) {
 	IDT[0x21].zero = 0;
 	IDT[0x21].type_attr = INTERRUPT_GATE;
 	IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
-	
+	/*
 	pit_address = (unsigned long)pit_handler;
 	IDT[0x22].offset_lowerbits = pit_address & 0xffff;
 	IDT[0x22].selector = KERNEL_CODE_SEGMENT_OFFSET;
 	IDT[0x22].zero = 0;
 	IDT[0x22].type_attr = INTERRUPT_GATE;
 	IDT[0x22].offset_higherbits = (pit_address & 0xffff0000) >> 16;
-	
+	*/
 	div0_address = (unsigned long)div0_handler;
 	IDT[0x00].offset_lowerbits = div0_address & 0xffff;
 	IDT[0x00].selector = KERNEL_CODE_SEGMENT_OFFSET;
