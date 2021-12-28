41d900aa58a64543a1e3db848741c6c695aba8b3
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-28 16:17:19 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 echo "NOTE: This requires QEMU installed, and an X server as well."
+
 echo "Now Building..."
-rm kc.o
-rm kasm.o
+rm *.o
+rm -rf iso
+rm *iso
+
 git pull
+
 #nasm -f elf32 boot/boot.asm -o boot.o
+
 nasm -f elf32 kernel/kernel.asm -o kasm.o
 gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
+
 ld -m elf_i386 -T link.ld -o kernel.elf kasm.o kc.o #boot.o
+
+mkdir -p iso/boot/grub
+cp kernel.elf iso/boot/kernel.elf
+cp grub.cfg iso/boot/grub/grub.cfg
+grub-mkrescue -o os.iso iso
+
 echo "Build Complete. Now running in QEMU."
+
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -kernel kernel.elf
+qemu-system-i386 -cdrom os.iso
+
