a421608b533531bdbe248d51092f087fe6dfb9f4
Killaship <69988679+Killaship@users.noreply.github.com>
2021-12-29 14:36:56 -0500

    Update gdt.asm

--- kernel/gdt.asm
+++ kernel/gdt.asm
@@ -1,53 +1,35 @@
-; GDT - Global Descriptor Table
-gdt_start:
-gdt_null:   ; Entry 1: Null entry must be included first (error check)
-    dd 0x0  ; double word = 4 bytes = 32 bits
-    dd 0x0
-gdt_code:   ; Entry 2: Code segment descriptor
-    ; Structure:
-    ; Segment Base Address (base) = 0x0
-    ; Segment Limit (limit) = 0xfffff
-    dw 0xffff   ; Limit bits 0-15
-    dw 0x0000   ; Base bits 0-15
-    db 0x00     ; Base bits 16-23
-    ; Flag Set 1:
-        ; Segment Present: 0b1
-        ; Descriptor Privilege level: 0x00 (ring 0)
-        ; Descriptor Type: 0b1 (code/data)
-    ; Flag Set 2: Type Field
-        ; Code: 0b1 (this is a code segment)
-        ; Conforming: 0b0 (Code w/ lower privilege may not call this)
-        ; Readable: 0b1 (Readable or execute only? Readable means we can read code constants)
-        ; Accessed: 0b0 (Used for debugging and virtual memory. CPU sets bit when accessing segment)
-    db 10011010b    ; Flag set 1 and 2
-    ; Flag Set 3
-        ; Granularity: 0b1 (Set to 1 multiplies limit by 4K. Shift 0xfffff 3 bytes left, allowing to span full 32G of memory)
-        ; 32-bit default: 0b1
-        ; 64-bit segment: 0b0
-        ; AVL: 0b0
-    db 11001111b    ; Flag set 3 and limit bits 16-19
-    db 0x00     ; Base bits 24-31
+gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
+    ; the GDT starts with a null 8-byte
+    dd 0x0 ; 4 byte
+    dd 0x0 ; 4 byte
+
+; GDT for code segment. base = 0x00000000, length = 0xfffff
+; for flags, refer to os-dev.pdf document, page 36
+gdt_code: 
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0       ; segment base, bits 0-15
+    db 0x0       ; segment base, bits 16-23
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x0       ; segment base, bits 24-31
+
+; GDT for data segment. base and length identical to code segment
+; some flags changed, again, refer to os-dev.pdf
 gdt_data:
-    ; Same except for code flag:
-        ; Code: 0b0
-    dw 0xfffff  ; Limit bits 0-15
-    dw 0x0000   ; Base bits 0-15
-    db 0x00     ; Base bits 16-23
-    db 10010010b    ; Flag set 1 and 2
-    db 11001111b    ; 2nd flags and limit bits 16-19
-    db 0x00     ; Base bits 24-31
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
 
-gdt_end:        ; Needed to calculate GDT size for inclusion in GDT descriptor
+gdt_end:
 
-; GDT Descriptor
+; GDT descriptor
 gdt_descriptor:
-    dw gdt_end - gdt_start - 1  ; Size of GDT, always less one
-    dd gdt_start
+    dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
+    dd gdt_start ; address (32 bit)
 
-; Define constants
+; define some constants for later use
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
-
-; In protected mode, set DS = INDEX to select GDT entries
-; Then CPU knows to use segment at that offset
-; Example: (0x0: NULL segment; 0x8: CODE segment; 0x10: DATA segment)
