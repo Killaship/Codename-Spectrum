4bc2cb107818d430b9338f883438597103ede04e
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-22 16:38:44 -0500

    Create cpu.h

---
+++ kernel/cpu.h
@@ -0,0 +1,14 @@
+static inline int cpuid_string(int code, int where[4]) {
+  __asm__ volatile ("cpuid":"=a"(*where),"=b"(*(where+0)),
+               "=d"(*(where+1)),"=c"(*(where+2)):"a"(code));
+  return (int)where[0];
+}
+ 
+const char * const cpu_string() {
+	static char s[16] = "BogusProces!";
+	cpuid_string(0, (int*)(s));
+	return s;
+}
+
+
+
