c004bb736356307887982e447928795f15f22a8c
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 14:52:13 -0500

    make rpibuild.sh work with grub

--- rpibuild.sh
+++ rpibuild.sh
@@ -1,13 +1,26 @@
 echo "NOTE: This requires QEMU installed, and an X server as well."
+
 echo "Now Building..."
-rm kc.o
-rm kasm.o
+rm *.o
+rm -rf iso
+rm *iso
+
 git pull
+
 #nasm -f elf32 boot/boot.asm -o boot.o
-nasm -f elf32 kernel/kernel.asm -o kasm.o
-clang -target  i686-unknown-none -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
-gold -m elf_i386 -T link.ld -o kernel.elf kasm.o kc.o #boot.o
+cd kernel
+nasm -f elf32 kernel.asm -o kasm.o
+cd -
+clang -target i686-unknown-none -Wall -Wextra -fno-stack-protector -c kernel/kernel.c -o kc.o
+
+gold -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o #boot.o
+
+mkdir -p iso/boot/grub
+cp kernel.elf iso/boot/kernel.elf
+cp grub.cfg iso/boot/grub/grub.cfg
+grub-mkrescue -o os.iso iso
 
 echo "Build Complete. Now running in QEMU."
+
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -curses -kernel kernel.elf
+qemu-system-i386 -cdrom os.iso -rtc base=localtime -curses
