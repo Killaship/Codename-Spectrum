d7836e591a737b968576e2b61e8e9f2fef970307
noobes <68382500+n00bes@users.noreply.github.com>
2022-01-24 00:09:53 +0200

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -2,3 +2,7 @@
 
 char read_port(unsigned short port);
 void write_port(unsigned short port, unsigned char data);
+
+//cpu.c
+static inline int cpuid_string(int code, int where[4]);
+const char * const cpu_string();
