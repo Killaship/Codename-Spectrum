a1906413751cfc64734b706bf628c09f0cbd56e5
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 14:06:34 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -280,8 +280,8 @@ void kmain(void) {
 	kprint("0",0xFF);
 	kprint_newline();
 	
-	const char *str = "Codename Spectrum Build 0.5.0";
-	const char *str2 = "                        Now with GRUB!";
+	const char *str = "Codename Spectrum Build 0.5.1";
+	const char *str2 = "                Now with more features! (but still no shell)";
 	kprint(str, 0x0B);
 	kprint_newline();
 	kprint(str2, 0x0E);
