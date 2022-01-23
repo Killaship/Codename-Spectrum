9ca200c0ebc298febde2fc2a94e9dddbaf375813
noobes <68382500+n00bes@users.noreply.github.com>
2022-01-24 00:11:29 +0200

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -6,3 +6,12 @@ void write_port(unsigned short port, unsigned char data);
 //cpu.c
 static inline int cpuid_string(int code, int where[4]);
 const char * const cpu_string();
+
+//panic.c
+void hang();
+void div0_handler();
+void debg_handler();
+void nmi_handler();
+void overf_handler();
+void boundrx_handler();
+void dfault_handler();
