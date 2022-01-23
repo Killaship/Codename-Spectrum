b480b7ae67fbd8771620c4c1f5e27ee762c4bfc8
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 17:03:07 -0500

    fix shit

--- build.sh
+++ build.sh
@@ -10,13 +10,10 @@ git pull
 #nasm -f elf32 boot/boot.asm -o boot.o
 cd kernel
 nasm -f elf32 kernel.asm -o kasm.o
-
-gcc -Wall -Wextra -fno-stack-protector -m32 kernel.c rtc.c cpu.c panic.c tty.c
-
-
-
-ld -m elf_i386 -T ../link.ld -o kernel.elf kasm.o kc.o rtc.o cpu.o panic.o tty.o
 cd -
+gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
+
+ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o #boot.o
 
 mkdir -p iso/boot/grub
 cp kernel.elf iso/boot/kernel.elf
@@ -27,4 +24,3 @@ echo "Build Complete. Now running in QEMU."
 
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
 qemu-system-i386 -cdrom os.iso -rtc base=localtime #-curses
-
