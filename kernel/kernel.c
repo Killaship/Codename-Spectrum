3e1087dbb57a4c15d5a97e955e33a8c497f3ab0b
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 17:57:03 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -83,8 +83,7 @@ void idt_init(void) {
  
 	unsigned long idt_address;
 	unsigned long idt_ptr[2];
-	kprint("Initializing IDT...", 0x07);
-	kprint_newline();
+
 	/* populate IDT entry of keyboard's interrupt */
 	keyboard_address = (unsigned long)keyboard_handler;
 	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
@@ -143,9 +142,6 @@ void idt_init(void) {
 	IDT[0x08].type_attr = INTERRUPT_GATE;
 	IDT[0x08].offset_higherbits = (dfault_address & 0xffff0000) >> 16;
  	
-	kprint("Initialized IDT entries!", 0x07);
-	kprint_newline();
- 
 	/*     Ports
 	*	 PIC1	PIC2
 	*Command 0x20	0xA0
@@ -183,8 +179,7 @@ void idt_init(void) {
 	idt_ptr[1] = idt_address >> 16 ;
 
 	load_idt(idt_ptr);
-	kprint("Loaded IDT!", 0x07);
-	kprint_newline();
+
 }
 
 
