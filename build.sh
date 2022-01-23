409119390d160cd91aaba97ac01f6ec465d0d1c1
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 13:56:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,5 +23,5 @@ grub-mkrescue -o os.iso iso
 echo "Build Complete. Now running in QEMU."
 
 export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -cdrom os.iso -curses
+qemu-system-i386 -cdrom os.iso 
 
