e6361b0f899a3418b6da7ea9208a70b1f5af4e5e
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 12:39:23 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -286,11 +286,11 @@ void kmain(void) {
     	read_rtc();
     	asm ("sti");
 	kprint("System Time: ",0x07);
-	kprint(get_RTC_register(0x04),0x07);
+	kprint(itoa(get_RTC_register(0x04)),0x07);
 	kprint(":",0x07);
-	kprint(get_RTC_register(0x02),0x07);
+	kprint(itoa(get_RTC_register(0x02)),0x07);
 	kprint(":",0x07);
-	kprint(get_RTC_register(0x00),0x07);
+	kprint(itoa(get_RTC_register(0x00)),0x07);
 	
 	sh_init();
 	while(1);
