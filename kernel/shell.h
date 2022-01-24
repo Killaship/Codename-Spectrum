1250d72dc02f0ab2f4cc8ffe179bee9496f2f86f
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-23 20:59:38 -0500

    Create shell.h

---
+++ kernel/shell.h
@@ -0,0 +1,29 @@
+#pragma once
+
+void input_prompt() {
+	kprint("Spectrum@fakeshell: ", 0x09);
+}
+
+void cmdflush() {
+	
+}
+
+void shell() {
+	kprint("Prism Shell [Kernelspace]", 0x07);
+	kprint_newline();
+	kprint("Type \"help\" for a list of commands.", 0x07);
+	kprint_newline();
+	kprint_newline();
+	input_prompt();
+	kprint(getchar(),0x07);
+	kprint_newline();
+	while(1) {
+
+	}
+}
+
+void sh_init() {
+	kprint_newline();
+	kprint_newline();
+	shell();
+}
