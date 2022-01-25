0ca3475ac8bc016ffc1230c84397793b3ebf5ded
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-25 07:53:04 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -211,8 +211,34 @@ char* itoa(int i)
 
 #include "shell.h"
 
+#define PORT 0x3f8          // COM1
+ 
+static int serial_init() {
+   write_port(PORT + 1, 0x00);    // Disable all interrupts
+   write_port(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+   write_port(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+   write_port(PORT + 1, 0x00);    //                  (hi byte)
+   write_port(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
+   write_port(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+   write_port(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+   write_port(PORT + 4, 0x1E);    // Set in loopback mode, test the serial chip
+   write_port(PORT + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
+ 
+   // Check if serial is faulty (i.e: not same byte as sent)
+   if(read_port(PORT + 0) != 0xAE) {
+      return 1;
+   }
+ 
+   // If serial is not faulty set it in normal operation mode
+   // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+   write_port(PORT + 4, 0x0F);
+   return 0;
+}
+
+
 void kmain(void) {
 	clear_screen();
+	serial_init();
 	idt_init();
 	kb_init();
 	kprint_newline();
