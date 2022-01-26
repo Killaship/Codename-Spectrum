44a5d770932b5e9b49ea4c86e42eaf0fa6817d04
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-26 08:23:17 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ git pull
 cd kernel
 nasm -f elf32 kernel.asm -o kasm.o
 cd -
-gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o
+gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
 
 ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o #boot.o
 
