22e06da6cbb38dc052f00e916c1d03ed4e78953e
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-25 20:51:06 -0500

    Update panic.h

--- kernel/panic.h
+++ kernel/panic.h
@@ -20,7 +20,7 @@ void div0_handler() {
 	kprint_newline();
 	kprint("err type:",0x40);
 	kprint_newline();
-	kprint("fault: divide-by-zero (0x00)",0x40);
+	kprint("fault: divide-by-zero (0x00) #DE",0x40);
 	kprint_newline();
 	kprint_newline();
 	hang();
@@ -37,7 +37,7 @@ void debg_handler() {
 	kprint_newline();
 	kprint("err type:",0x40);
 	kprint_newline();
-	kprint("fault/trap: debug (0x01)",0x40);
+	kprint("fault/trap: debug (0x01) #DB",0x40);
 	kprint_newline();
 	kprint_newline();
 	hang();
@@ -71,7 +71,7 @@ void overf_handler() {
 	kprint_newline();
 	kprint("err type:",0x40);
 	kprint_newline();
-	kprint("trap: overflow detected (0x04)",0x40);
+	kprint("trap: overflow detected (0x04) #OF",0x40);
 	kprint_newline();
 	kprint_newline();
 	hang();
@@ -89,7 +89,7 @@ void boundrx_handler() {
 	kprint_newline();
 	kprint("err type:",0x40);
 	kprint_newline();
-	kprint("fault: bound range exceeded (0x05)",0x40);
+	kprint("fault: bound range exceeded (0x05) #BR",0x40);
 	kprint_newline();
 	kprint_newline();
 	hang();
@@ -106,7 +106,7 @@ void dfault_handler() {
 	kprint_newline();
 	kprint("err type:",0x40);
 	kprint_newline();
-	kprint("abort: double fault (0x08)",0x40);
+	kprint("abort: double fault (0x08) #DF",0x40);
 	kprint_newline();
 	kprint_newline();
 	kprint_newline();
@@ -115,3 +115,21 @@ void dfault_handler() {
 	hang();
 }
 
+
+
+void badtss_handler() {
+	kprint_newline();
+	unsigned int i = 0;
+	while (i < SCREENSIZE) {
+		vidptr[i++] = ' ';
+		vidptr[i++] = 0x44;
+	}
+	kprint("err: kernel panic!",0x40);
+	kprint_newline();
+	kprint("err type:",0x40);
+	kprint_newline();
+	kprint("fault: bad tss #TS (0x0A)",0x40);
+	kprint_newline();
+	kprint_newline();
+	hang();
+}
