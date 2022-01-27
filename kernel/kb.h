67725e18f5f42dab29c4ce9304c221187a006586
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-27 17:21:36 -0500

    Update kb.h

--- kernel/kb.h
+++ kernel/kb.h
@@ -54,7 +54,7 @@ void keyboard_handler_main(void) {
 		}		
 		
 		last_char = keyboard_map[(char) keycode];
-		kprint(keyboard_map[(char) keycode];,0x07);
+		kprint(keyboard_map[(char) keycode],0x07);
 		kprint(last_char, 0x07);
 
 		
