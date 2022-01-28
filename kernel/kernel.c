ee9749ef6c22d7b2f712b5ba9bea3b935fa0814c
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-28 10:46:30 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -269,28 +269,7 @@ void idt_init(void) {
 
 
 
-char* itoa(int i)
-{
-      static char text[12];
-      int loc = 11;
-      text[11] = 0;
-      char neg = 1;
-      if (i >= 0)
-      {
-         neg = 0;
-         i = -i;
-      }
-      while (i)
-      {
-          text[--loc] = '0' - (i%10);
-          i/=10;
-      }
-      if (loc==11)
-          text[--loc] = '0';
-      if (neg)
-         text[--loc] = '-';      
-      return &text[loc];
-}
+
 
 #include "shell.h"
 
