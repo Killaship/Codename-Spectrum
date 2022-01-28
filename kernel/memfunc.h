1379f7511eebf1f4b7f903f70ed1f830a175f81a
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-28 10:46:51 -0500

    Update memfunc.h

--- kernel/memfunc.h
+++ kernel/memfunc.h
@@ -50,3 +50,26 @@ void* memmove(void* dstptr, const void* srcptr, size_t size) {
 	}
 	return dstptr;
 }
+
+char* itoa(int i)
+{
+      static char text[12];
+      int loc = 11;
+      text[11] = 0;
+      char neg = 1;
+      if (i >= 0)
+      {
+         neg = 0;
+         i = -i;
+      }
+      while (i)
+      {
+          text[--loc] = '0' - (i%10);
+          i/=10;
+      }
+      if (loc==11)
+          text[--loc] = '0';
+      if (neg)
+         text[--loc] = '-';      
+      return &text[loc];
+}
