7808bc23be890a087069df21e100a5fae32362de
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-30 15:44:17 -0500

    Update libc.h

--- kernel/libc.h
+++ kernel/libc.h
@@ -60,7 +60,7 @@ int strncmp(const char *s1, const char *s2, size_t n)
 		}
 		else
 			return s1[count] - s2[count];
-	}
+	}}
 
 int memcmp(const void* aptr, const void* bptr, size_t size) {
 	const unsigned char* a = (const unsigned char*) aptr;
