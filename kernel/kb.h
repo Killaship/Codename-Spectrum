6f6fbe76aa82ef4dbdf522b2ca7cdc50f56451f1
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-30 15:32:51 -0500

    Update kb.h

--- kernel/kb.h
+++ kernel/kb.h
@@ -68,7 +68,7 @@ void keyboard_handler_main(void) {
 		buffer[i] = c[0];
 		i++;
     		kprint(c,0x07);
-		if(strcmp(buffer,"time") == 0) {
+		if(strcmp(buffer, "time") == 0) {
 			printtime();
 		}
 		
