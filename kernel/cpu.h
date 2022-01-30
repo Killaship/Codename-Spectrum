adc01b29acc6c141ee93beee092fb63cd060abe8
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-30 15:43:49 -0500

    Update cpu.h

--- kernel/cpu.h
+++ kernel/cpu.h
@@ -1,5 +1,5 @@
 #pragma once
-inline int cpuid_string(int code, int where[4]) {
+static inline int cpuid_string(int code, int where[4]) {
   __asm__ volatile ("cpuid":"=a"(*where),"=b"(*(where+0)),
                "=d"(*(where+1)),"=c"(*(where+2)):"a"(code));
   return (int)where[0];
