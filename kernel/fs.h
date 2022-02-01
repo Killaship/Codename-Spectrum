31cf10124b9e934af376dfbb6093c54db2c04438
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-01 15:41:43 -0500

    this is probably a bad idea

---
+++ kernel/fs.h
@@ -0,0 +1,14 @@
+/* returns file size and pointer to file data in out */
+int tar_lookup(unsigned char *archive, char *filename, char **out) {
+    unsigned char *ptr = archive;
+ 
+    while (!memcmp(ptr + 257, "ustar", 5)) {
+        int filesize = oct2bin(ptr + 0x7c, 11);
+        if (!memcmp(ptr, filename, strlen(filename) + 1)) {
+            *out = ptr + 512;
+            return filesize;
+        }
+        ptr += (((filesize + 511) / 512) + 1) * 512;
+    }
+    return 0;
+}
