34acd488f9ea19759356d6207e0a5904d0d1dae3
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-01 21:10:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo "Now Building..."
 rm *.o
 rm -rf iso
 rm *iso
+git pull
 
 tar cvf initrd.tar ramdisk
-
-git pull
+ld -r -b binary initrd.tar -o initrd.o
 
 #nasm -f elf32 boot/boot.asm -o boot.o
 cd kernel
@@ -15,7 +15,7 @@ nasm -f elf32 kernel.asm -o kasm.o
 cd -
 gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
 
-ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o #boot.o
+ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o -format binary initrd.o
 
 mkdir -p iso/boot/grub
 cp kernel.elf iso/boot/kernel.elf
