14efc0afcbaee8b2eab86168569f847a566e5f54
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-01 20:14:31 -0500

    Update fs.h

--- kernel/fs.h
+++ kernel/fs.h
@@ -1,14 +1,55 @@
-/* returns file size and pointer to file data in out */
-int tar_lookup(unsigned char *archive, char *filename, char **out) {
-    unsigned char *ptr = archive;
+struct tar_header
+{
+    char filename[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char chksum[8];
+    char typeflag[1];
+};
+
+unsigned int getsize(const char *in)
+{
  
-    while (!memcmp(ptr + 257, "ustar", 5)) {
-        int filesize = oct2bin(ptr + 0x7c, 11);
-        if (!memcmp(ptr, filename, strlen(filename) + 1)) {
-            *out = ptr + 512;
-            return filesize;
-        }
-        ptr += (((filesize + 511) / 512) + 1) * 512;
+    unsigned int size = 0;
+    unsigned int j;
+    unsigned int count = 1;
+ 
+    for (j = 11; j > 0; j--, count *= 8)
+        size += ((in[j - 1] - '0') * count);
+ 
+    return size;
+ 
+}
+
+struct tar_header *headers[32];
+
+unsigned int parse(unsigned int address)
+{
+ 
+    unsigned int i;
+ 
+    for (i = 0; ; i++)
+    {
+ 
+        struct tar_header *header = (struct tar_header *)address;
+ 
+        if (header->name[0] == '\0')
+            break;
+ 
+        unsigned int size = getsize(header->size);
+ 
+        headers[i] = header;
+ 
+        address += ((size / 512) + 1) * 512;
+ 
+        if (size % 512)
+            address += 512;
+ 
     }
-    return 0;
+ 
+    return i;
+ 
 }
