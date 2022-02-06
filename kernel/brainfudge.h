5a12683f8f775d45a1377c5ae2b21b218bd846b0
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-06 18:12:53 -0500

    Update brainfudge.h

--- kernel/brainfudge.h
+++ kernel/brainfudge.h
@@ -1,26 +1,41 @@
-// This is free and unencumbered software released into the public domain.
+// initialize the tape with 30,000 zeroes
+unsigned char tape[30000] = {0};
+ 
+// set the pointer to point at the left-most cell of the tape
+unsigned char* ptr = tape;
 
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
 
-// In jurisdictions that recognize copyright laws, the author or authors
-// of this software dedicate any and all copyright interest in the
-// software to the public domain. We make this dedication for the benefit
-// of the public at large and to the detriment of our heirs and
-// successors. We intend this dedication to be an overt act of
-// relinquishment in perpetuity of all present and future rights to this
-// software under copyright law.
+void interpret(char* input) {
+    char current_char;
+    size_t i;
+    size_t loop;
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-// 
-// For more information, please refer to <http://unlicense.org/>
-#include <fcntl.h>
-putchar(char);c,f;main(int n,char**b){if (n<2)return 0;f=open(b[1], O_RDONLY);while(read(f,&c,1)==1)putchar(c);close(f);return main(n-1,b+1);}
+    for (i = 0; input[i] != 0; i++) {
+        current_char = input[i];
+        if (current_char == '>') {
+            ++ptr;
+        } else if (current_char == '<') {
+            --ptr;
+        } else if (current_char == '+') {
+            ++*ptr;
+        } else if (current_char == '-') {
+            --*ptr;
+        } else if (current_char == '.' ) {
+            putchar(*ptr);
+        } else if (current_char == ',') {
+            *ptr = getchar();
+        } else if (current_char == '[') {
+            continue;
+        } else if (current_char == ']' && *ptr) {
+            loop = 1;
+            while (loop > 0) {
+                current_char = input[--i];
+                if (current_char == '[') {
+                    loop--;
+                } else if (current_char == ']') {
+                    loop++;
+                }
+            }
+        }
+    }
+}
