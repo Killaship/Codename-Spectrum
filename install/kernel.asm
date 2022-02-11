bf92ca6d25d35f218eef5ae1832f21f858bd791b
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-11 07:22:53 -0500

    Update kernel.asm

--- install/kernel.asm
+++ install/kernel.asm
@@ -9,6 +9,30 @@ section .text
         dd 0x00                    ;flags
         dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
 
+gdt_start:
+	dq 0x0
+gdt_code:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10011010b
+	db 11001111b
+	db 0x0
+gdt_data:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+gdt_end:
+gdt_pointer:
+	dw gdt_end - gdt_start
+	dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 global start
 global keyboard_handler
 global read_port
@@ -41,10 +65,16 @@ keyboard_handler:
 	iretd
 
 start:
-	cli 				;block interrupts
-	mov esp, stack_space
-	call kmain
-	hlt 				;halt the CPU
+     lgdt [gdt_pointer]  
+     jmp CODE_SEG:kmain
+    .setcs:
+    mov ax, DATA_SEG          ; Setup the segment registers with our flat data selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, stack_space        ; set stack pointer			;halt the CPU
 
 section .bss
 resb 8192; 8KB for stack
