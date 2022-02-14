ff90903dc4bfaea6dd2a7e3f52a0e7bd90ab6a0e
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-14 07:04:51 -0500

    Update kb.h

--- kernel/kb.h
+++ kernel/kb.h
@@ -115,7 +115,7 @@ void keyboard_handler_main(void) {
 			else if(strcmp("test", buffer) == 0) {
 				kprint_newline();
 				kprint("I can taste 43 and hear purple",0x09); // :p
-				memcpy((void*) 0xb8000, (void*) 0x2000, 0xFFFF); 
+				memcpy((void*) 0xb8000, (void*) 0x1001, 0xFFFF); 
 			}			
 			else if(strcmp("cls", buffer) == 0) {
 				clear_screen();
