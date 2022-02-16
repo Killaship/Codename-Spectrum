6749e4dc5520e89771a259a03fa172c497dc06c3
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-16 08:09:26 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,23 +1,32 @@
 
-echo "Now Building..."
+
 rm *.o
 rm -rf iso
 rm *iso
-
 git pull
 
-#nasm -f elf32 boot/boot.asm -o boot.o
+
+
 cd kernel
 nasm -f elf32 kernel.asm -o kasm.o
 cd -
 gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
 
-ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o #boot.o
+cd install
+nasm -fbin kernel.asm -o iasm.bin
+cd -
+
+
+
+ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o 
+
+
 
 mkdir -p iso/boot/grub
 cp kernel.elf iso/boot/kernel.elf
+cp install.bin iso/boot/install.bin
 cp grub.cfg iso/boot/grub/grub.cfg
 grub-mkrescue -o os.iso iso
 
-echo "Build Complete. See os.iso."
 
+echo "Done!"
