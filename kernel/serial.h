04390ed2ad76570b43793def925c9c9550a6516c
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-11 14:26:34 -0500

    Update serial.h

--- kernel/serial.h
+++ kernel/serial.h
@@ -41,3 +41,25 @@
         write_port(SERIAL_DATA_PORT(com),
              divisor & 0x00FF);
     }
+    
+        /** serial_configure_line:
+     *  Configures the line of the given serial port. The port is set to have a
+     *  data length of 8 bits, no parity bits, one stop bit and break control
+     *  disabled.
+     *
+     *  @param com  The serial port to configure
+     */
+    void serial_configure_line(unsigned short com)
+    {
+        /* Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
+         * Content: | d | b | prty  | s | dl  |
+         * Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
+         */
+        write_port(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+    }
+    
+        int serial_is_transmit_fifo_empty(unsigned int com)
+    {
+        /* 0x20 = 0010 0000 */
+        return read_port(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
+    }
