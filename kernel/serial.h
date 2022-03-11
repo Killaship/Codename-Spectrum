2a9c4f1d63ad09395868599c559fa70bd2515477
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-11 14:23:43 -0500

    Create serial.h

---
+++ kernel/serial.h
@@ -0,0 +1,43 @@
+#pragma once 
+
+
+    /* The I/O ports */
+
+    /* All the I/O ports are calculated relative to the data port. This is because
+     * all serial ports (COM1, COM2, COM3, COM4) have their ports in the same
+     * order, but they start at different values.
+     */
+
+    #define SERIAL_COM1_BASE                0x3F8      /* COM1 base port */
+
+    #define SERIAL_DATA_PORT(base)          (base)
+    #define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+    #define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+    #define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+    #define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+
+    /* The I/O port commands */
+
+    /* SERIAL_LINE_ENABLE_DLAB:
+     * Tells the serial port to expect first the highest 8 bits on the data port,
+     * then the lowest 8 bits will follow
+     */
+    #define SERIAL_LINE_ENABLE_DLAB         0x80
+
+    /** serial_configure_baud_rate:
+     *  Sets the speed of the data being sent. The default speed of a serial
+     *  port is 115200 bits/s. The argument is a divisor of that number, hence
+     *  the resulting speed becomes (115200 / divisor) bits/s.
+     *
+     *  @param com      The COM port to configure
+     *  @param divisor  The divisor
+     */
+    void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
+    {
+        write_port(SERIAL_LINE_COMMAND_PORT(com),
+             SERIAL_LINE_ENABLE_DLAB);
+        write_port(SERIAL_DATA_PORT(com),
+             (divisor >> 8) & 0x00FF);
+        write_port(SERIAL_DATA_PORT(com),
+             divisor & 0x00FF);
+    }
