8ff642ad1d26034da98011a23cc03836605e9f4a
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-12 09:13:14 -0500

    Update program.asm

--- kernel/program.asm
+++ kernel/program.asm
@@ -1,7 +1,11 @@
 ; external program to be loaded by the kernel later on
 ; set eax to some distinguishable number, to read from the log afterwards
 mov eax, 0xDEADBEEF
+jmp stop
 ; enter infinite loop, nothing more to do
 ; $ means "beginning of line", ie. the same instruction
-jmp $
+stop:
+  cli
+  hlt
+  jmp stop
 
