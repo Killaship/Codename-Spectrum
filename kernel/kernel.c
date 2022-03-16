5c6c93c0e0a0fd994e90ee32a2bae154d81ae15c
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-16 18:00:06 -0400

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -42,7 +42,19 @@ void write_port(unsigned short port, unsigned char data) {
 #include "rtc.h"
 #include "mem.h"
 
-
+syscall_handler(unsigned int ebx) {
+	switch(ebx) {
+		case 0x01:
+			kprint("test1",0x07);
+			break;
+		case 0x02:
+			kprint_newline();
+			break;
+		case 0x03:
+			kprint("test2",0x07);
+			break;
+	}
+}
 
 
 
@@ -73,6 +85,7 @@ void idt_init(void) {
 	unsigned long reserved_address;
 	unsigned long ss_address;
 	unsigned long gp_address;
+	unsigned long syscall_address;
  
 	
 	
@@ -111,6 +124,13 @@ void idt_init(void) {
 	IDT[0x01].type_attr = INTERRUPT_GATE;
 	IDT[0x01].offset_higherbits = (debg_address & 0xffff0000) >> 16;
  
+	syscall_address = (unsigned long)syscall_handler;
+	IDT[0x01].offset_lowerbits = syscall_address & 0xffff;
+	IDT[0x01].selector = KERNEL_CODE_SEGMENT_OFFSET;
+	IDT[0x01].zero = 0;
+	IDT[0x01].type_attr = INTERRUPT_GATE;
+	IDT[0x01].offset_higherbits = (syscall_address & 0xffff0000) >> 16;
+	
 	nmi_address = (unsigned long)nmi_handler;
 	IDT[0x02].offset_lowerbits = nmi_address & 0xffff;
 	IDT[0x02].selector = KERNEL_CODE_SEGMENT_OFFSET;
