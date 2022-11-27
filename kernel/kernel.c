a6a9a1e6a1e5c26e08792717ea213f53c02e3973
William Batts III <69988679+Killaship@users.noreply.github.com>
2022-11-27 18:37:08 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,52 +3,43 @@
 * License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
 */
 #define my_sizeof(type) (char *)(&type+1)-(char*)(&type)
+// (C) Killaship 2022, lol
+
+#include "multiboot.h"
 #include <stdint.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdarg.h>
+#include <ctype.h>
+extern void keyboard_handler(void);
+#include "keyboard_map.h"
 #include "common.h"
-#define IDT_SIZE 256
-#define INTERRUPT_GATE 0x8e
-#define IDT_TA_CallGate         0b10001100
-#define IDT_TA_TrapGate         0b10001111
-#define KERNEL_CODE_SEGMENT_OFFSET 0x08
+#include "ports.h"
+#include "tty.h"
+#include "panic.h"
+#include "memory.h"
+#include "cpu.h"
+#include "rtc.h"
+#include "kb.h"
+#include "idt.h"
 #define osversion "Codename Spectrum v0.6.2"
 
 
 extern void keyboard_handler(void);
-
 #include "keyboard_map.h"
-#include "tty.h"
-
-#include "panic.h"
-#include "cpu.h"
-#include "rtc.h"
-#include "mem.h"
-
-void printcpu() {
-	kprint("Vendor ID: ", 0x07);
-	kprint(cpu_string(), 0x0C);
-}
-
-void printtime() {
-	asm volatile ("cli");
-    	read_rtc();
-    	asm volatile ("sti");
-	
-	kprint("System Time: ",0x07);
-	kprint(itoa(hour),0x07);
-	kprint(":",0x07);
-	kprint(itoa(minute),0x07);
-	kprint(":",0x07);
-	kprint(itoa(second),0x07);
-}
-
 #include "kb.h"
+
+
 
 
 void kmain(void) {
 	clear_screen();
 	idt_init();
+	kprint("IDT init\n", 0x07);
+	timer_phase(100);
+	kprint("PIT init\n", 0x07);
 	kb_init();
+	kprint("KB init\n", 0x07);
 	sh_init();
 	while(1);
 }
