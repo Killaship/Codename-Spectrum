3655f08cfd3b888072fc8cfc8c88ad88ae977e6d
William Batts III <69988679+Killaship@users.noreply.github.com>
2022-11-27 18:38:39 -0500

    Replace build.sh with YAOS script (fancier!)

--- build.sh
+++ build.sh
@@ -1,40 +1,66 @@
 
+fs() {
+	echo "compiling"
+	gcc -Wall -Wextra -fno-stack-protector -m32 -g -c src/programs/testA.c -o testA.o -ffreestanding
+	echo "linking"
+	ld -Tlink.ld -g -m elf_i386 -o testA.elf testA.o
+	#smlrl -flat32 -origin 0x80000 -o testA.bin testA.o
+	echo "convertifying"
+	objcopy -O binary testA.elf testA.bin
+	xxd -i testA.bin src/testA.h
+}
 
-echo "Now Building..."
-rm *.o
-rm -rf iso
-rm *iso
-git pull
-
-
-
-cd kernel
-nasm -f elf32 kernel.asm -o kasm.o
-nasm -f bin program.asm -o program
-cd -
-gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
-
-cd install
-nasm -fbin install.asm -o install.bin
-cd -
+compile() {
 
 
+	cd src
+	nasm -f elf32 kernel.asm -o kasm.o
+	cd -
 
-ld -m elf_i386 -T link.ld -o kernel.elf kernel/kasm.o kc.o 
+	gcc -Wall -Wextra -fno-stack-protector -m32 -g -c src/kernel.c -o kc.o -ffreestanding
 
+	ld -m elf_i386 -T link.ld -g -o kernel.elf src/kasm.o kc.o 
+	mkdir -p iso/boot/grub
+	mkdir -p iso/modules
+	cp kernel.elf iso/boot/kernel.elf
+	cp grub.cfg iso/boot/grub/grub.cfg
+	grub-mkrescue -o os.iso iso
+}
 
+run() {
+	qemu-system-i386 -cdrom os.iso -rtc base=localtime -curses 
+}
+help() {
+	echo "Usage:"
+	echo "-r            || Compile the YAOS source code, and then runs it in QEMU."
+	echo "-n            || Just runs YAOS in QEMU. Requires os.iso to be present from prior compilings."
+	echo "-c            || Just compiles the YAOS source code, without running it."
+	echo "anything else, or -h || Shows this help text."
 
-mkdir -p iso/boot/grub
-mkdir -p iso/modules
+}
+# Get the options
+while getopts ":nrc" option; do
+   case $option in
+      h) # help message
+		 help
+		 exit;;
+      r) # compile and run
+		 fs
+         compile
+         run
+         exit;;
+      c) # just compile
+		 fs
+      	 compile
+      	 exit;;
+      n) # just run
+      	 run
+      	 exit;;
 
-cp kernel.elf iso/boot/kernel.elf
-cp kernel/program iso/modules/program
-cp install/install.bin iso/boot/install.bin
-cp grub.cfg iso/boot/grub/grub.cfg
+      \?) # invalid option, shows help message
+		 help
+		 exit;;
+                             
+      esac
+done
 
-grub-mkrescue -o os.iso iso
-
-echo "Build Complete. Now running in QEMU."
-
-export DISPLAY=:0.0 # Replace 0.0 with the address of your X server if needed.
-qemu-system-i386 -cdrom os.iso -rtc base=localtime -curses
